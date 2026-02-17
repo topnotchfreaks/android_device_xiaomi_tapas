4c98007765dbe52ab1d03124af5cb6c758110063
Liekoo <veyeka1@gmail.com>
2026-02-17 14:19:04 +0800

    tapas: Cleanup Scripts

--- rootdir/bin/init.class_main.sh
+++ rootdir/bin/init.class_main.sh
@@ -38,7 +38,6 @@ low_ram=`getprop ro.config.low_ram`
 case "$baseband" in
     "apq" | "sda" | "qcs" )
     setprop ro.vendor.radio.noril yes
-    stop vendor.ril-daemon
     stop vendor.qcrild
     stop vendor.qcrild2
     stop vendor.qcrild3
@@ -87,16 +86,4 @@ case "$baseband" in
         *)
             ;;
     esac
-esac
-
-#
-# Allow persistent faking of bms
-# User needs to set fake bms charge in persist.vendor.bms.fake_batt_capacity
-#
-fake_batt_capacity=`getprop persist.vendor.bms.fake_batt_capacity`
-case "$fake_batt_capacity" in
-    "") ;; #Do nothing here
-    * )
-    echo "$fake_batt_capacity" > /sys/class/power_supply/battery/capacity
-    ;;
 esac
