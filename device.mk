2c6ad77bd88e6034795f16f0f988ac576c83e36f
Liekoo <veyeka1@gmail.com>
2026-02-17 22:06:22 +0800

    tapas: import public.libraries.txt from stock ROM

--- device.mk
+++ device.mk
@@ -454,6 +454,10 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-xiaomi)
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
