0eb22337fe8fe5b1444394804c558f08fd01dcf0
Liekoo <veyeka1@gmail.com>
2026-02-17 21:44:54 +0800

    Revert "tapas: Remove QTI perf service"
    This reverts commit e9256188b2bd94e2dd8b5ada4a5971a19c359028.

--- device.mk
+++ device.mk
@@ -430,8 +430,13 @@ PRODUCT_PACKAGES += \
      vendor_firmware_mnt_mountpoint
 
 # Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.3.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
 # Platform
 TARGET_BOARD_PLATFORM := bengal
