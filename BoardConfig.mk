7514200fa282f35fde906bf1ae1451a502768bc7
env. <162432464+belowzeroiq@users.noreply.github.com>
2026-06-18 22:10:29 +0900

    Revert "topaz: Move to xiaomi common sepolicy rules"
    This reverts commit f89d228d03200cd129d0879bacd82da6ecacff3c.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,7 +174,6 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-include device/xiaomi/sepolicy/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 #SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/dynamic
