5afff7b9e10752c69ff3e718fc5051961d358251
env. <162432464+belowzeroiq@users.noreply.github.com>
2026-07-12 09:59:14 +0900

    tapas: Drop Lineage LiveDisplay and Health service configurations

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,7 +177,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/xiaomi/sepolicy/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 #SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/dynamic
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/dynamic
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
