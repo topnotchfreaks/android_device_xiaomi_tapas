6c03c356beeead81563f0fda6064e3423214cb54
Liekoo <veyeka1@gmail.com>
2026-07-17 20:17:38 +0800

    tapas: power-libperfmgr: Update included soong namespaces

--- device.mk
+++ device.mk
@@ -526,7 +526,8 @@ PRODUCT_SHIPPING_API_LEVEL := $(SHIPPING_API_LEVEL)
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel \
+    hardware/google/pixel/pixelstats \
+    hardware/google/pixel/power-libperfmgr \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
