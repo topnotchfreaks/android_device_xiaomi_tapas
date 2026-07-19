51b943b260dea7361a4ff00fdec6ae99bee99c7a
env. <162432464+belowzeroiq@users.noreply.github.com>
2026-07-19 18:00:24 +0900

    tapas: ASCP!

--- tapas.mk
+++ tapas.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/tapas/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 
-PRODUCT_NAME := lineage_tapas
+PRODUCT_NAME := tapas
 PRODUCT_DEVICE := tapas
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
