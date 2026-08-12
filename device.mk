d9dcdbbb302de18c8f6fa4e581ffb5074ac3da88
env. <162432464+belowzeroiq@users.noreply.github.com>
2026-08-13 00:41:40 +0900

    tapas: add init.tapas_perf.sh for IRQ tuning

--- device.mk
+++ device.mk
@@ -314,6 +314,7 @@ PRODUCT_PACKAGES += \
     init.qti.early_init.sh \
     init.qti.kernel.sh \
     init.qti.write.sh \
+    init.tapas_perf.sh \
     system_dlkm_modprobe.sh \
     vendor_modprobe.sh
 
