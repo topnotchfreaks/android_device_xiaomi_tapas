d4f148eda4b400f27e800c530940f5983ec53ebc
zenin1504 <kayozzprojects@yandex.com>
2026-09-13 01:23:24 +0800

    tapas: Build KProfiles and label it properly
    Change-Id: I5744eaff16754929bdd6f3a8c8be8cd7
    Signed-off-by: zenin1504 <me.zenin1504.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -358,6 +358,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+
+# KProfiles
+PRODUCT_PACKAGES += \
+    KProfiles
+
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
